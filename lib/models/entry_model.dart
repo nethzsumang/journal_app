bdca5a81cb1813da620156d204029bac7a79525f
Kenneth Sumang <dev@kennethsumang.com>
2023-12-26 22:02:57 +0800

    wip: dismissible list

---
+++ lib/models/entry_model.dart
@@ -0,0 +1,8 @@
+class EntryModel {
+  final int entryId;
+  final String title;
+  final String content;
+
+  EntryModel(
+      {required this.entryId, required this.title, required this.content});
+}
--- lib/view/screens/entry_list_screen.dart
+++ lib/view/screens/entry_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:journal_app/models/entry_model.dart';
+import 'package:journal_app/view/widgets/headers/h4_text_widget.dart';
 
 class EntryListPage extends StatefulWidget {
   const EntryListPage({super.key, required this.title});
@@ -10,6 +12,22 @@ class EntryListPage extends StatefulWidget {
 }
 
 class _EntryListPageState extends State<EntryListPage> {
+  final ScrollController _scrollController = ScrollController();
+  final sampleEntries = [
+    EntryModel(entryId: 1, title: 'Entry 1', content: 'Entry 1 content'),
+    EntryModel(entryId: 2, title: 'Entry 2', content: 'Entry 2 content'),
+    EntryModel(entryId: 3, title: 'Entry 3', content: 'Entry 3 content'),
+    EntryModel(entryId: 4, title: 'Entry 4', content: 'Entry 4 content'),
+    EntryModel(entryId: 5, title: 'Entry 5', content: 'Entry 5 content'),
+    EntryModel(entryId: 6, title: 'Entry 6', content: 'Entry 6 content'),
+    EntryModel(entryId: 7, title: 'Entry 7', content: 'Entry 7 content'),
+    EntryModel(entryId: 8, title: 'Entry 8', content: 'Entry 8 content'),
+    EntryModel(entryId: 9, title: 'Entry 9', content: 'Entry 9 content'),
+    EntryModel(entryId: 10, title: 'Entry 10', content: 'Entry 10 content'),
+    EntryModel(entryId: 11, title: 'Entry 11', content: 'Entry 11 content'),
+    EntryModel(entryId: 12, title: 'Entry 12', content: 'Entry 12 content'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +35,50 @@ class _EntryListPageState extends State<EntryListPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Text('Body Page'),
+      body: Center(
+        child: Scrollbar(
+          controller: _scrollController,
+          child: ListView.builder(
+            itemCount: sampleEntries.length,
+            controller: _scrollController,
+            itemBuilder: (context, index) {
+              final entry = sampleEntries[index];
+              return Column(
+                children: [
+                  Dismissible(
+                    key: Key(entry.entryId.toString()),
+                    background: Container(
+                      color: Colors.white,
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 16),
+                          child: Icon(Icons.pin),
+                        ),
+                      ),
+                    ),
+                    secondaryBackground: Container(
+                      color: Colors.white,
+                      child: const Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 16),
+                          child: Icon(Icons.delete),
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      title: H4TextWidget(text: entry.title),
+                      subtitle: Text(entry.content),
+                    ),
+                  ),
+                  const Divider(color: Colors.black54)
+                ],
+              );
+            },
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () => {}, child: const Icon(Icons.add)),
     );
